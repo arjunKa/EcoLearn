61ebdfb00730505d4243f6d34bfce1936c1916f2
arjunKa <32141878+arjunKa@users.noreply.github.com>
2024-04-07 17:31:39 -0400

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -22,7 +22,7 @@ from ecoLearning import views as b2
 
 #these are api tools for admin access, to add new entires and delete them
 router = routers.DefaultRouter()
-router.register(r'trees', b2.TreeView, 'Tree')
+router.register('trees', b2.TreeView, basename = 'tree')
 router.register(r'gardens', b2.GardenView, 'Garden')
 router.register(r'vehicles', b2.VehicleView, 'Vehicle')
 router.register(r'food', b2.FoodView, 'Food')
@@ -34,7 +34,7 @@ urlpatterns = [
     path('api/ecolearning/trees', b2.tree_list, name = "trees"),
     path('api/ecolearning/gardens', b2.garden, name = "gardens"),
     path('api/ecolearning/vehicles', b2.vehicle, name = "vehicles"),
-        path('api/ecolearning/food', b2.food, name = "vehicles"),
+    path('api/ecolearning/food', b2.food, name = "vehicles"),
     # re_path('api/ecolearning/$', b2.tree_list),
     # re_path(r'^api/ecolearning/([0-9])$', b2.TreeView.as_view({'get': 'tree'}))
     
