d3594423c345aef8d5766e1c276fad3bc434249d
arjunKa <32141878+arjunKa@users.noreply.github.com>
2024-04-07 19:47:42 -0400

    test

--- frontend/src/components/Axios.jsx
+++ frontend/src/components/Axios.jsx
@@ -4,7 +4,7 @@ const isDevelopment = import.meta.env.MODE === 'development'
 const baseUrl2 = isDevelopment ? import.meta.env.VITE_API_BASE_URL_LOCAL : import.meta.env.VITE_API_BASE_URL_PROD
 
 const AxiosInstance = axios.create({
-    baseURL: baseUrl2, 
+    baseURL: 'http://ecolearn.azurewebsites.net', 
     timeout: 5000, 
     headers: {
         "Content-Type": "application/json", 
