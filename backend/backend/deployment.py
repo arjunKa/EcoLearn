31f8c0e3b8b383979395f0e9a197acf806459ffa
arjunKa <32141878+arjunKa@users.noreply.github.com>
2024-04-07 17:55:20 -0400

    updated urls

--- backend/backend/deployment.py
+++ backend/backend/deployment.py
@@ -1,4 +1,4 @@
-import os
+import os 
 from .settings import *
 from .settings import BASE_DIR
 
@@ -21,8 +21,8 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     'https://delightful-sky-0f5eaf70f.5.azurestaticapps.net'
-    
 ]
+
 
 STORAGES = {
     "default": {
@@ -43,8 +43,39 @@ DATABASES = {
         "HOST": CONNECTION_STR['host'],
         "USER": CONNECTION_STR['user'],
         "PASSWORD": CONNECTION_STR['password'],
-        
     }
 }
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    },
+}
+
+
+
+ADMINS = [("Nick", "YOURMAIL.com")]
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+DEFAULT_FROM_EMAIL = 'default from email'
+
+
 
 STATIC_ROOT = BASE_DIR/'staticfiles'
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -22,10 +22,10 @@ from ecoLearning import views as b2
 
 #these are api tools for admin access, to add new entires and delete them
 router = routers.DefaultRouter()
-router.register('trees', b2.TreeView, basename = 'tree')
-router.register(r'gardens', b2.GardenView, 'Garden')
-router.register(r'vehicles', b2.VehicleView, 'Vehicle')
-router.register(r'food', b2.FoodView, 'Food')
+router.register('trees', b2.TreeView, basename = 'trees')
+router.register('gardens', b2.GardenView, basename = 'gardens')
+router.register('vehicles', b2.VehicleView, basename = 'vehicles')
+router.register('food', b2.FoodView, basename = 'food')
 
 urlpatterns = [
     
@@ -34,7 +34,7 @@ urlpatterns = [
     path('api/ecolearning/trees', b2.tree_list, name = "trees"),
     path('api/ecolearning/gardens', b2.garden, name = "gardens"),
     path('api/ecolearning/vehicles', b2.vehicle, name = "vehicles"),
-    path('api/ecolearning/food', b2.food, name = "vehicles"),
+    path('api/ecolearning/food', b2.food, name = "food"),
     # re_path('api/ecolearning/$', b2.tree_list),
     # re_path(r'^api/ecolearning/([0-9])$', b2.TreeView.as_view({'get': 'tree'}))
     
